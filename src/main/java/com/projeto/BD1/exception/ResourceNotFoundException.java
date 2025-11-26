152af899a574973e2547b648fe54acc1cb7005ec
Jean Vitor Vieira <jeanvitorv0@unesc.net>
2025-11-25 21:54:44 -0300

    Adicionando exceptions

---
+++ src/main/java/com/projeto/BD1/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.projeto.BD1.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
